0ed6055f228f535879d395df4c587dbd611f0266
MadeByDroids <9824702+MadeByDroids@users.noreply.github.com>
2026-01-07 14:30:58 +0000

    js update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollSmoother) 
 var myEase = Power3.easeOut; 
+
+ScrollSmoother.create({
+	smooth: 1,
+	effects: true
+});
 
 const leftItem = gsap.utils.toArray('.from-left');
         leftItem.forEach((leftyitem, i) => {
@@ -42,7 +49,7 @@ const fadeIn = gsap.utils.toArray('.fadeIn');
 
 const scaleIn = gsap.utils.toArray('.scaleIn');
         scaleIn.forEach((scaleItemIn, i) => {
-          const anim = gsap.fromTo(scaleItemIn, {scale:0.5, autoAlpha: 0 }, { duration: 4, scale:1, autoAlpha: 1, delay:0.55});
+          const anim = gsap.fromTo(scaleItemIn, {scale:0.75, autoAlpha: 0 }, { duration: 4, scale:1, autoAlpha: 1, delay:0.75});
           ScrollTrigger.create({
             trigger: scaleItemIn,
             animation: anim,
@@ -71,11 +78,11 @@ const baseItem = gsap.utils.toArray('.from-base');
         
 
 // video fader
-gsap.set(".video_contain", { opacity: 0.4, duration: 1, }); // Set initial opacity to 0
+gsap.set(".video_contain", { opacity: 0.3, duration: 1, }); // Set initial opacity to 0
 
 gsap.to("html", {
   scrollTrigger: { 
-    trigger: ".intro",
+    trigger: ".introt",
     toggleActions: "restart none none reverse"
 },
   "--myBlur": 14,
@@ -85,10 +92,10 @@ gsap.to("html", {
 
 gsap.to (".video_contain", {
   scrollTrigger: { 
-    trigger: ".intro",
+    trigger: ".introt",
     toggleActions: "restart none none reverse"
 },
-  opacity:0.2,
+  opacity:0.1,
   duration: 1.25,
   
 });    
@@ -106,6 +113,8 @@ gsap.to (".topbar", {
   duration: 0.25,
   
 }); 
+
+
 
 
 PowerGlitch.glitch(
@@ -294,6 +303,8 @@ const render = () => {
 render()
 
 
+// BALL FOLLOWER
+
 gsap.set(".ball", {xPercent: -50, yPercent: -50});
 
 var ball = document.querySelector(".ball");
@@ -310,6 +321,7 @@ window.addEventListener("mousemove", e => {
   mouse.x = e.x;
   mouse.y = e.y;  
 });
+
 
 gsap.ticker.add((time, deltaTime) => {
   
@@ -335,7 +347,7 @@ vid.pause();
 }
 
 
-// Get the modal ---------------
+// Get the showreel modal ---------------
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -350,6 +362,7 @@ btn.onclick = function() {
   modal.style.display = "block";
   gsap.to ("#myModal", {  opacity:1, duration: 0.5,});  
   playVid();
+  document.body.classList.add("noscroll"); // Disable scrolling
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -370,3 +383,211 @@ window.onclick = function(event) {
   }
 }
 
+
+// Casestudy 1 modal ---------------
+var caseOne = document.getElementById("csOne");
+
+// Get the button that opens the modal
+var caseOneBtn = document.getElementById("caseOneBtn");
+
+// Get the <span> element that closes the modal
+var caseOneClose = document.getElementsByClassName("csCloseOne")[0];
+
+// When the user clicks on the button, open the modal
+caseOneBtn.onclick = function() {
+  gsap.set(caseOne, { opacity: 0, duration: 0, }); // Set initial opacity to 0
+  caseOne.style.display = "block";
+  gsap.to (caseOne, {  opacity:1, duration: 0.5,});  
+  document.body.classList.add("noscroll"); // Disable scrolling
+}
+
+// When the user clicks on <span> (x), close the modal
+caseOneClose.onclick = function() {
+  gsap.to (caseOne, {  opacity:0, duration: 0.25,  onComplete: () => {
+  caseOne.style.display = "none";
+  document.body.classList.remove("noscroll"); // Enable scrolling
+  }});  
+}
+
+
+// Casestudy 2 modal ---------------
+var caseTwo = document.getElementById("csTwo");
+
+// Get the button that opens the modal
+var caseTwoBtn = document.getElementById("caseTwoBtn");
+
+// Get the <span> element that closes the modal
+var caseTwoClose = document.getElementsByClassName("csCloseTwo")[0];
+
+// When the user clicks on the button, open the modal
+caseTwoBtn.onclick = function() {
+  gsap.set(caseTwo, { opacity: 0, duration: 0, }); // Set initial opacity to 0
+  caseTwo.style.display = "block";
+  gsap.to (caseTwo, {  opacity:1, duration: 0.5,});  
+  document.body.classList.add("noscroll"); // Disable scrolling
+}
+
+// When the user clicks on <span> (x), close the modal
+caseTwoClose.onclick = function() {
+  gsap.to (caseTwo, {  opacity:0, duration: 0.25,  onComplete: () => {
+  caseTwo.style.display = "none";
+  document.body.classList.remove("noscroll"); // Enable scrolling
+  }});  
+}
+
+
+// Casestudy 3 modal ---------------
+var caseThree = document.getElementById("csThree");
+
+// Get the button that opens the modal
+var caseThreeBtn = document.getElementById("caseThreeBtn");
+
+// Get the <span> element that closes the modal
+var caseThreeClose = document.getElementsByClassName("csCloseThree")[0];
+
+// When the user clicks on the button, open the modal
+caseThreeBtn.onclick = function() {
+  gsap.set(caseThree, { opacity: 0, duration: 0, }); // Set initial opacity to 0
+  caseThree.style.display = "block";
+  gsap.to (caseThree, {  opacity:1, duration: 0.5,});  
+  document.body.classList.add("noscroll"); // Disable scrolling
+}
+
+// When the user clicks on <span> (x), close the modal
+caseThreeClose.onclick = function() {
+  gsap.to (caseThree, {  opacity:0, duration: 0.25,  onComplete: () => {
+  caseThree.style.display = "none";
+  document.body.classList.remove("noscroll"); // Enable scrolling
+  }});  
+}
+
+
+// Casestudy 4 modal ---------------
+var caseFour = document.getElementById("csFour");
+
+// Get the button that opens the modal
+var caseFourBtn = document.getElementById("caseFourBtn");
+
+// Get the <span> element that closes the modal
+var caseFourClose = document.getElementsByClassName("csCloseFour")[0];
+
+// When the user clicks on the button, open the modal
+caseFourBtn.onclick = function() {
+  gsap.set(caseFour, { opacity: 0, duration: 0, }); // Set initial opacity to 0
+  caseFour.style.display = "block";
+  gsap.to (caseFour, {  opacity:1, duration: 0.5,});  
+  document.body.classList.add("noscroll"); // Disable scrolling
+}
+
+// When the user clicks on <span> (x), close the modal
+caseFourClose.onclick = function() {
+  gsap.to (caseFour, {  opacity:0, duration: 0.25,  onComplete: () => {
+  caseFour.style.display = "none";
+  document.body.classList.remove("noscroll"); // Enable scrolling
+  }});  
+}
+
+// TURN CASE ON FOR EDIT ---------------
+//gsap.set(caseThree, { opacity: 1, duration: 0, }); // Set initial opacity to 0
+//caseThree.style.display = "block";
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+console.log('Page fully loaded');
+
+// SPLIT TEXT CONTACT TEXT
+
+// Select the text section and the title element
+var textSection = document.querySelector('.textsec');
+var title = document.querySelector(".titlechr");
+
+// Create a SplitText instance to split the title into words and characters
+var h1SplitText = new SplitText(title, 
+    { 
+        type: "words,chars" 
+    }
+);
+// Get an array of all the characters in the splitted element
+var spliTextChars = h1SplitText.chars;
+
+// Set a perspective on the title for a 3D effect
+//gsap.set(title, { perspective: 400 });
+
+// Create a timeline for the animation
+var splitTextTimeline = gsap.timeline({
+    defaults: { 
+        duration: 5, // Duration of the animation in seconds
+        ease: "sine.out", // Type of easing
+    },
+});
+
+// Add a "from" animation to the timeline to make the characters appear
+splitTextTimeline.from(spliTextChars, 
+    { 
+        opacity:0,    // Initial opacity: 0 (invisible)
+        y: 20,      // Initial vertical offset: 180px upwards
+        stagger: 0.1, // Delay between each character's animation
+    }
+);
+
+// Create a ScrollTrigger instance to trigger the animation on scroll
+ScrollTrigger.create({
+    trigger: textSection,         // Element that triggers the animation
+    start: '0% 60%',            // Animation start position 
+    end: '90% 80%',               // Animation end position 
+    scrub: true,                  // Enable scrubbing for a smoother effect
+    animation: splitTextTimeline, // Link the timeline to ScrollTrigger
+    //markers:true,                 // Show ScrollTrigger markers for debugging
+}); 
+
+
+
+//--------- SECOND
+
+// Select the text section and the title element
+var textSectionT = document.querySelector('.textsecT');
+var titleT = document.querySelector(".titlechrT");
+
+// Create a SplitText instance to split the title into words and characters
+var h1SplitTextT = new SplitText(titleT, 
+    { 
+        type: "words,chars" 
+    }
+);
+// Get an array of all the characters in the splitted element
+var spliTextCharsT = h1SplitTextT.chars;
+
+// Set a perspective on the title for a 3D effect
+//gsap.set(title, { perspective: 400 });
+
+// Create a timeline for the animation
+var splitTextTimelineT = gsap.timeline({
+    defaults: { 
+        duration: 5, // Duration of the animation in seconds
+        ease: "sine.out", // Type of easing
+    },
+});
+
+// Add a "from" animation to the timeline to make the characters appear
+splitTextTimelineT.from(spliTextCharsT, 
+    { 
+        opacity:0,    // Initial opacity: 0 (invisible)
+        y: 0,      // Initial vertical offset: 180px upwards
+        stagger: 0.5, // Delay between each character's animation
+    }
+);
+
+// Create a ScrollTrigger instance to trigger the animation on scroll
+ScrollTrigger.create({
+    trigger: textSectionT,         // Element that triggers the animation
+    start: '-30% 70%',            // Animation start position 
+    end: '260% 90%',                // Animation end position 
+    scrub: true,                  // Enable scrubbing for a smoother effect
+    animation: splitTextTimelineT, // Link the timeline to ScrollTrigger
+    //markers:true,                 // Show ScrollTrigger markers for debugging
+}); 
+
+});
